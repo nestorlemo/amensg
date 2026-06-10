a9d88f6df427230863f658ff7b4a9d8b68c19848
Lemo Estrada <nlemo@bse.com.uy>
2026-06-10 14:24:42 -0300

    fix: correct casing in index.ts exports

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,8 +1,7 @@
-@"
 export { Badge } from './Badge'
 export { Button } from './button'
 export { Card } from './card'
 export { Input } from './input'
 export { ModalShell } from './Modal'
 export { Select } from './select'
-"@ | Set-Content components/ui/index.ts
+
